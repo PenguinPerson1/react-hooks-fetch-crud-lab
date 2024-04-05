7a226b1d9d63f1ad89c9ba79515906dab65801dc
Annania Plossl <nia.plossl@gmail.com>
2024-04-05 15:14:42 -0400

    completed the lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,37 @@
-import React, { useState } from "react";
+import React, {useEffect,useState} from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
-
+  const [questions,setQuestions] = useState([])
+  useEffect(()=>{
+    fetch("http://localhost:4000/questions")
+    .then(r=>r.json())
+    .then(questions => setQuestions(questions))
+    .catch(error=>console.log(error))
+  },[])
+  function updateQuestions(newQuestion){
+    setQuestions([...questions,newQuestion])
+  }
+  function deleteItem(itemId){
+    setQuestions(questions.filter(question => question.id !== itemId))
+  }
+  function changeItem(item) {
+    setQuestions(questions.map(question=> {
+      if (question.id === item.id) {
+        return item
+      }
+      return question
+    }))
+  }
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? 
+        <QuestionForm updateQuestions={updateQuestions}  /> : 
+        <QuestionList questions={questions} deleteItem={deleteItem} changeItem={changeItem} />}
     </main>
   );
 }
--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,13 +1,30 @@
 import React from "react";
 
 function QuestionItem({ question }) {
-  const { id, prompt, answers, correctIndex } = question;
+  const { id, prompt, answers, correctIndex, deleteItem, changeItem } = question;
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
       {answer}
     </option>
   ));
+  function handleDelete() {
+    fetch(`http://localhost:4000/questions/${id}`,{
+      method: "DELETE"
+    })
+    .then(()=> deleteItem(id) )
+  }
+  function handleChange(event) {
+    fetch(`http://localhost:4000/questions/${id}`,{
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ "correctIndex" : event.target.value })
+    })
+    .then(r=>r.json)
+    .then(updated=> changeItem(updated))
+    .catch(error=>console.log(error))
+  }
+
 
   return (
     <li>
@@ -15,9 +32,9 @@ function QuestionItem({ question }) {
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleChange}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
